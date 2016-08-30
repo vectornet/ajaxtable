7d53c8494e8647361964553dbc9dd532aeffde86
Joubert RedRat <me+github@redrat.com.br>
2016-08-30 11:55:19 -0300

    Add API documentation

---
+++ docs/api/0.2/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","VectorDev\\AjaxTable\\Cell"],["c","VectorDev\\AjaxTable\\Col"],["c","VectorDev\\AjaxTable\\Column"],["c","VectorDev\\AjaxTable\\Conf"],["c","VectorDev\\AjaxTable\\Config"],["c","VectorDev\\AjaxTable\\Configuration"],["c","VectorDev\\AjaxTable\\Format\\Classes"],["c","VectorDev\\AjaxTable\\Format\\Format"],["c","VectorDev\\AjaxTable\\Format\\Table"],["c","VectorDev\\AjaxTable\\Lang\\Lang"],["c","VectorDev\\AjaxTable\\Lang\\PtBr"],["c","VectorDev\\AjaxTable\\Response"],["c","VectorDev\\AjaxTable\\Row"]];
